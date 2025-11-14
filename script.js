95bd5a1246b07f58964823c024e99dd941822a1b
Accio-1 <rahulwanjare291@gmail.com>
2025-11-14 18:58:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function resetGrid() {
+    for (let i = 1; i <= 9; i++) {
+        document.getElementById(i).style.backgroundColor = "transparent";
+    }
+}
+
+document.getElementById("change_button").addEventListener("click", function () {
+    const blockID = document.getElementById("block_id").value;
+    const color = document.getElementById("colour_id").value;
+
+    resetGrid(); 
+
+    if (blockID >= 1 && blockID <= 9) {
+        document.getElementById(blockID).style.backgroundColor = color;
+    }
+});
+
+document.getElementById("Reset").addEventListener("click", function () {
+    resetGrid();
+});
 
